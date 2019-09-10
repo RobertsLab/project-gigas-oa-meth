2454cf0e3279d1c07956fe713f0a81a5fe21c36a
Yaamini Venkataraman <yaaminiv@uw.edu>
2019-09-10 09:30:35 -0700

    changed wall time

--- scripts/2019-09-03-Bismark.sh
+++ scripts/2019-09-03-Bismark.sh
@@ -8,7 +8,7 @@
 ## Nodes
 #SBATCH --nodes=1
 ## Walltime (days-hours:minutes:seconds format)
-#SBATCH --time=00-15:00:00
+#SBATCH --time=3-00:00:00
 ## Memory per node
 #SBATCH --mem=120G
 ##turn on e-mail notification
