a0cb541d1e350c99c472fc4f35a7166bf626c6ea
Yaamini Venkataraman <yaaminiv@uw.edu>
2019-09-13 14:45:10 -0700

    new script deduplication onwards with proper wildcard

---
+++ scripts/2019-09-12-Bismark-Deduplication.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=20190903_bismark_yaamini
+## Allocation Definition
+#SBATCH --account=srlab
+#SBATCH --partition=srlab
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=3-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yaaminiv@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/yaamini/analyses/Gigas-WGBS/2019-09-11-Gigas-Bismark
+
+#Deduplication
+
+source /gscratch/srlab/programs/scripts/paths.sh
+
+/gscratch/srlab/programs/Bismark-0.21.0/deduplicate_bismark \
+--samtools_path /gscratch/srlab/programs/samtools-1.9/ \
+-p \
+--bam \
+YRV*_R1_001_bismark_bt2_pe.bam
+
+#Sorting for Downstream Applications
+
+find *deduplicated.bam \
+| xargs basename -s _R1_001_bismark_bt2_pe.deduplicated.bam | xargs -I{} /gscratch/srlab/programs/samtools-1.9/samtools \
+sort {}_R1_001_bismark_bt2_pe.deduplicated.bam \
+-o {}_dedup.sorted.bam
+
+#Indexing for Downstream Applications
+
+find *dedup.sorted.bam \
+| xargs basename -s _dedup.sorted.bam | xargs -I{} /gscratch/srlab/programs/samtools-1.9/samtools \
+index {}_dedup.sorted.bam
+
+#Methylation Extraction
+
+/gscratch/srlab/programs/Bismark-0.21.0/bismark_methylation_extractor \
+--samtools_path /gscratch/srlab/programs/samtools-1.9/ \
+-p \
+--bedGraph \
+--counts \
+--scaffolds \
+--multicore 4 \
+*deduplicated.bam
+
+#HTML Processing Report
+
+/gscratch/srlab/programs/Bismark-0.21.0/bismark2report
+
+#Summary Report
+
+/gscratch/srlab/programs/Bismark-0.21.0/bismark2summary
