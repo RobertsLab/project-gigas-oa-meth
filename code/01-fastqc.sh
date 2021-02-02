e692b31ee2ba659ffcb9210b81b80e481cff5ab3
Yaamini Venkataraman <yaaminiv@uw.edu>
2021-02-01 22:23:13 -0800

    adapted script from sam

--- code/01-fastqc.sh
+++ code/01-fastqc.sh
@@ -17,24 +17,117 @@
 ## Specify the working directory for this job
 #SBATCH --chdir=/gscratch/scrubbed/yaaminiv/Manchester/analyses/fastqc
 
-#Exit script if any command fails
+# Script adapted from one used by Sam White: https://robertslab.github.io/sams-notebook/2020/11/10/FastQC-MultiQC-C.gigas-Ploidy-WGBS-Raw-Sequence-Data-from-Ronits-Project-on-Mox.html
 
+###################################################################################
+
+# These variables need to be set by user
+
+# FastQC output directory
+output_dir=/gscratch/scrubbed/yaaminiv/Manchester/analyses/fastqc
+
+# Set number of CPUs to use
+threads=28
+
+# Input/output files
+checksums=fastq_checksums.md5
+fastq_list=fastq_list.txt
+raw_reads_dir=/gscratch/scrubbed/yaaminiv/Manchester/data/
+
+# Paths to programs
+fastqc=/gscratch/srlab/programs/fastqc_v0.11.9/fastqc
+multiqc=/gscratch/srlab/programs/anaconda3/bin/multiqc
+
+# Programs associative array
+declare -A programs_array
+programs_array=(
+[fastqc]="${fastqc}" \
+[multiqc]="${multiqc}"
+)
+
+###################################################################################
+
+# Exit script if any command fails
 set -e
-
-# Directories and programs
-reads_dir="/gscratch/scrubbed/yaaminiv/Manchester/data/"
-fastqc_dir="/gscratch/srlab/programs/fastqc_v0.11.9/"
-source /gscratch/srlab/programs/scripts/paths.sh
 
 # Load Python Mox module for Python module availability
 module load intel-python3_2017
 
-#FastQC: Assess raw sequence quality
-find ${reads_dir}* \
-| ${fastqc_dir}fastqc \
---outdir /gscratch/scrubbed/yaaminiv/Manchester/analyses/fastqc \
---threads 28
+# Sync raw FastQ files to working directory
+rsync --archive --verbose \
+"${raw_reads_dir}"zr3616*.fq.gz .
 
-#MultiQC: Combine reports
-/gscratch/srlab/programs/anaconda3/bin/multiqc \
-/gscratch/scrubbed/yaaminiv/Manchester/analyses/fastqc/.
+# Populate array with FastQ files
+fastq_array=(*.fq.gz)
+
+# Pass array contents to new variable
+fastqc_list=$(echo "${fastq_array[*]}")
+
+# Run FastQC
+# NOTE: Do NOT quote ${fastqc_list}
+${programs_array[fastqc]} \
+--threads ${threads} \
+--outdir ${output_dir} \
+${fastqc_list}
+
+# Create list of fastq files used in analysis
+echo "${fastqc_list}" | tr " " "\n" >> ${fastq_list}
+
+# Generate checksums for reference
+while read -r line
+do
+
+	# Generate MD5 checksums for each input FastQ file
+	echo "Generating MD5 checksum for ${line}."
+	md5sum "${line}" >> "${checksums}"
+	echo "Completed: MD5 checksum for ${line}."
+	echo ""
+
+	# Remove fastq files from working directory
+	echo "Removing ${line} from directory"
+	rm "${line}"
+	echo "Removed ${line} from directory"
+	echo ""
+done < ${fastq_list}
+
+# Run MultiQC
+${programs_array[multiqc]} .
+
+
+# Capture program options
+for program in "${!programs_array[@]}"
+do
+	{
+  echo "Program options for ${program}: "
+	echo ""
+  # Handle samtools help menus
+  if [[ "${program}" == "samtools_index" ]] \
+  || [[ "${program}" == "samtools_sort" ]] \
+  || [[ "${program}" == "samtools_view" ]]
+  then
+    ${programs_array[$program]}
+  fi
+	${programs_array[$program]} -h
+	echo ""
+	echo ""
+	echo "----------------------------------------------"
+	echo ""
+	echo ""
+} &>> program_options.log || true
+
+  # If MultiQC is in programs_array, copy the config file to this directory.
+  if [[ "${program}" == "multiqc" ]]; then
+  	cp --preserve ~/.multiqc_config.yaml .
+  fi
+done
+
+
+# Document programs in PATH (primarily for program version ID)
+{
+date
+echo ""
+echo "System PATH for $SLURM_JOB_ID"
+echo ""
+printf "%0.s-" {1..10}
+echo "${PATH}" | tr : \\n
+} >> system_path.log
