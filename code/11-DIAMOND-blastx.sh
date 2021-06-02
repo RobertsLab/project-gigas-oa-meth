4ea167b8bc469bcf1620519f1b343cb10f3f2015
Yaamini <yaaminiv@uw.edu>
2021-06-01 21:13:14 -0700

    add long-reads argument

--- code/11-DIAMOND-blastx.sh
+++ code/11-DIAMOND-blastx.sh
@@ -30,9 +30,9 @@ diamond=/gscratch/srlab/programs/diamond-2.0.4/diamond
 # Save to srlab/yaamini/blastdbs
 
 ${diamond} makedb \
---in /gscratch/srlab/blastdbs/uniprot_sprot_20200123/uniprot_sprot.fasta \
+--in /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot_sprot.fasta \
 --threads 27 \
--d  /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot-sprot.dmnd
+-d /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot-sprot.dmnd
 
 # Run DIAMOND with blastx
 # Output format 6 produces a standard BLAST tab-delimited file
@@ -40,6 +40,7 @@ ${diamond} blastx \
 --db /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot-sprot.dmnd \
 --query /gscratch/scrubbed/yaaminiv/data/cgigas_uk_roslin_v1_genomic-mito.fa \
 --out 20210601-cgigas-roslin-mito-blastx.outfmt6 \
+--long-reads \
 --outfmt 6 \
 --evalue 1e-4 \
 --max-target-seqs 1 \
