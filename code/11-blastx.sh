820a38f79212e13457f07570d4bf14b2006a7fd7
Yaamini <yaaminiv@uw.edu>
2021-06-05 17:41:38 -0700

    rework normal blastx

--- code/11-blastx.sh
+++ code/11-blastx.sh
@@ -23,26 +23,24 @@
 set -e
 
 # Program paths
-diamond=/gscratch/srlab/programs/diamond-2.0.4/diamond
+blast=/gscratch/srlab/programs/ncbi-blast-2.10.1+/bin/
 
 # Create database from Uniprot-SwissProt information
-# Use 27 threads to create database
+# Speciy protein database
 # Save to srlab/yaamini/blastdbs
 
-${diamond} makedb \
---in /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot_sprot.fasta \
---threads 27 \
--d /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot-sprot.dmnd
+${blast}makeblastdb \
+-in /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot_sprot.fasta \
+-dbtype prot \
+-out /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot-sprot.blastdb
 
-# Run DIAMOND with blastx
+# Run blastx
 # Output format 6 produces a standard BLAST tab-delimited file
-${diamond} blastx \
---db /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot-sprot.dmnd \
---query /gscratch/scrubbed/yaaminiv/data/cgigas_uk_roslin_v1_genomic-mito.fa \
---out 20210601-cgigas-roslin-mito-blastx.outfmt6 \
---long-reads \
---outfmt 6 \
---evalue 1e-4 \
---max-target-seqs 1 \
---block-size 15.0 \
---index-chunks 4
+${blast}blastx \
+-db /gscratch/srlab/yaaminiv/blastdbs/20210601-uniprot-sprot.blastdb \
+-query /gscratch/scrubbed/yaaminiv/data/cgigas_uk_roslin_v1_genomic-mito.fa \
+-out 20210605-cgigas-roslin-mito-blastx.outfmt6 \
+-outfmt 6 \
+-evalue 1e-4 \
+-max_target_seqs 1 \
+-num_threads 28
